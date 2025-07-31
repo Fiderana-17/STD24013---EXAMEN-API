191f847873c6b809e00b23f8b715666e0ab8d11b
unknown <Fiderana Rakoto>
2025-07-31 08:41:20 +0300

    feat: add GET /home route returning HTML with welcome message + add postman capture Q2

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import json
 
 from fastapi import FastAPI
 from starlette.responses import Response
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+#Q1
 @app.get("/ping")
 def ping():
     return Response(content="pong", media_type="text/plain", status_code=200);
+
+#Q2
+@app.get("/home", response_class=HTMLResponse)
+async def home():
+    return "<h1>Welcome home!</h1>"
+

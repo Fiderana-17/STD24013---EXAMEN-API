2dc5a8ac4d11f8be0afad0c9f501656f0489fe1c
unknown <Fiderana Rakoto>
2025-07-31 09:49:44 +0300

    feat: implement POST, GET and PUT routes for /posts with in-memory storage

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
-from fastapi import FastAPI,Response
-from fastapi.responses import HTMLResponse
-
-
+from fastapi import FastAPI, Request, Response, status, HTTPException, Depends
+from fastapi.responses import HTMLResponse, PlainTextResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime
+import base64
 
 app = FastAPI()
 
@@ -22,3 +25,44 @@ def catch_all(full_path: str):
     with open("not_found.html", "r", encoding="utf-8") as file:
         html_content = file.read()
     return Response(content=html_content, status_code=200, media_type="text/html")
+
+
+
+# Mémoire vive pour stocker les posts
+posts_memory = []
+
+# Modèle de données pour un post
+class Post(BaseModel):
+    author: str
+    title: str
+    content: str
+    creation_datetime: datetime
+
+# Q4
+@app.post("/posts", status_code=201)
+def create_posts(new_posts: List[Post]):
+    posts_memory.extend(new_posts)
+    return posts_memory
+
+# Q5
+@app.get("/posts")
+def get_posts():
+    return posts_memory
+
+# Q6
+@app.put("/posts")
+def upsert_posts(updated_posts: List[Post]):
+    global posts_memory
+    updated_dict = {post.title: post for post in updated_posts}
+
+    new_memory = []
+    for post in posts_memory:
+        if post.title in updated_dict:
+            new_memory.append(updated_dict.pop(post.title))
+        else:
+            new_memory.append(post)
+
+    new_memory.extend(updated_dict.values())
+
+    posts_memory = new_memory
+    return posts_memory
